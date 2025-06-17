0a2ba142721b26d9312acb272c61adab3765f17e
ddadivan <30553969+ddadivan@users.noreply.github.com>
2025-06-17 18:52:01 +0300

    Add drag n drop, paginator, sort in head

--- projects/src/app/projects/company/components/tablet-employees/tablet-employees.component.ts
+++ projects/src/app/projects/company/components/tablet-employees/tablet-employees.component.ts
@@ -1,22 +1,24 @@
-import {Component, inject, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, inject, ViewChild} from '@angular/core';
 import {
-  CdkCell,
-  CdkCellDef,
-  CdkColumnDef,
-  CdkHeaderCell,
-  CdkHeaderCellDef,
-  CdkHeaderRow, CdkHeaderRowDef, CdkRow, CdkRowDef,
-  CdkTable
+    CdkCell,
+    CdkCellDef,
+    CdkColumnDef,
+    CdkHeaderCell,
+    CdkHeaderCellDef,
+    CdkHeaderRow, CdkHeaderRowDef, CdkRow, CdkRowDef,
+    CdkTable
 } from "@angular/cdk/table";
 import {IEmployee} from "../../interfaces/company.interfaces";
 import {UsersApiService} from "../../shared/users-api.service";
 import {MatCheckbox} from "@angular/material/checkbox";
 import {RouterLink} from "@angular/router";
 import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray} from "@angular/cdk/drag-drop";
-import {MatTable} from "@angular/material/table";
+import {MatTable, MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort, MatSortHeader, Sort} from "@angular/material/sort";
 
 @Component({
-  selector: 'app-tablet-employees',
+    selector: 'app-tablet-employees',
     imports: [
         CdkTable,
         CdkColumnDef,
@@ -31,46 +33,102 @@ import {MatTable} from "@angular/material/table";
         MatCheckbox,
         RouterLink,
         CdkDropList,
-        CdkDrag
+        CdkDrag,
+        MatPaginator,
+        MatSort,
+        MatSortHeader,
     ],
-  templateUrl: './tablet-employees.component.html',
-  styleUrl: './tablet-employees.component.scss'
+    templateUrl: './tablet-employees.component.html',
+    styleUrl: './tablet-employees.component.scss'
 })
-export class TabletEmployeesComponent {
+export class TabletEmployeesComponent implements AfterViewInit {
 
-  public UsersApiService: UsersApiService = inject(UsersApiService);
+    public UsersApiService: UsersApiService = inject(UsersApiService);
 
-  @ViewChild('table', {static: true}) table!: MatTable<IEmployee>;
+    @ViewChild('table', {static: true}) table!: MatTable<IEmployee>;
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
 
-  public displayedColumns: string[] = ['isChecked', 'name', 'email', 'departament', 'employWorkDate', 'action', 'info'];
+    public displayedColumns: string[] = ['isChecked', 'name', 'email', 'departament', 'employWorkDate', 'action', 'info'];
 
-  public dataSource: IEmployee[] = this.UsersApiService.employeesList();
+    public dataSource = new MatTableDataSource<IEmployee>(this.UsersApiService.employeesList());
+    public isShowViewMore: boolean = false;
+    public currentId: string | null = null;
 
-  public isShowViewMore: boolean = false;
-  public currentId: string | null = null;
+    public sortedData: IEmployee[] = this.dataSource.data.slice();
 
-  public viewMoreAction(id: string) {
+    ngAfterViewInit() {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
 
-    if (this.currentId === id) {
-      this.isShowViewMore = !this.isShowViewMore;
-    } else {
-      this.currentId = id;
-      this.isShowViewMore = true;
+        this.dataSource.sortingDataAccessor = (item: IEmployee, property: string) => {
+            switch (property) {
+                case 'name':
+                    return `${item.lastName} ${item.firstName}`;
+                case 'employWorkDate':
+                    return new Date(item.employWorkDate);
+                default:
+                    return (item as any)[property];
+            }
+        };
     }
-  }
 
-  public showMoreBlock(id: string): boolean {
-    return this.isShowViewMore && this.currentId === id;
-  }
+    public viewMoreAction(id: string) {
+        if (this.currentId === id) {
+            this.isShowViewMore = !this.isShowViewMore;
+        } else {
+            this.currentId = id;
+            this.isShowViewMore = true;
+        }
+    }
 
-  public viewMoreText(id: string): string {
-    return this.currentId === id && this.isShowViewMore ? 'Hide view' : 'View more';
-  }
+    public showMoreBlock(id: string): boolean {
+        return this.isShowViewMore && this.currentId === id;
+    }
 
-  public  drop(event: CdkDragDrop<string>) {
-     const previousIndex = this.dataSource.findIndex(d => d === event.item.data);
+    public viewMoreText(id: string): string {
+        return this.currentId === id && this.isShowViewMore ? 'Hide view' : 'View more';
+    }
 
-     moveItemInArray(this.dataSource, previousIndex, event.currentIndex);
-     this.table.renderRows();
-  }
+    public drop(event: CdkDragDrop<string>) {
+        const previousIndex = this.dataSource.data.findIndex(d => d === event.item.data);
+
+        moveItemInArray(this.dataSource.data, previousIndex, event.currentIndex);
+        this.dataSource.data = [...this.dataSource.data];
+    }
+
+    public sortData(sort: Sort) {
+        const data = this.sortedData.slice();
+        if (!sort.active || sort.direction === '') {
+            this.sortedData = data;
+            return;
+        }
+
+        this.sortedData = data.sort((a, b) => {
+            const isAsc = sort.direction === 'asc';
+            switch (sort.active) {
+                case 'name':
+                    return this.compare(`${a.firstName} ${a.lastName}`, `${b.firstName} ${b.lastName}`, isAsc);
+                case 'email':
+                    return this.compare(a.email, b.email, isAsc);
+                case 'departament':
+                    return this.compare(a.departament, b.departament, isAsc);
+                case 'employWorkDate':
+                    return this.sortDates(a, b, isAsc);
+                default:
+                    return 0;
+            }
+        });
+    }
+
+    private compare(a: number | string, b: number | string, isAsc: boolean) {
+        return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    }
+
+    private sortDates(a: IEmployee, b: IEmployee, isAsc: boolean): number {
+        const dateA = new Date(a.employWorkDate);
+        const dateB = new Date(b.employWorkDate);
+        return (dateA.getTime() < dateB.getTime() ? -1 : 1) * (isAsc ? 1 : -1);
+    }
+
 }
